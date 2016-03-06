a807741d7e0e050f289cd4159de210e5b8ce284b
KaminariNoKage <kaiblogs@hotmail.com>
2016-03-05 23:54:28 -0500

    Bunch more cells

--- dbs.py
+++ dbs.py
@@ -1,10 +1,19 @@
 item_db = {
     "pebble": {
-        "description": "A pebble found at the bottom of a lakebed",
-        "examination": "A pebble picked up",
+        "description": "A pebble",
+        "examination": "A pebble. It is rather smooth.",
         "actions": {
-            "throw": lambda args: print("Threw rock")
+            "throw": lambda args: print("Threw rock"),
+            "flirt": lambda args: print("The pebble is indifferent.")
         },
-        "weight": 0.1
+        "weight": 0.01
+    },
+    "log": {
+    	"description": "A log",
+    	"examination": "",
+    	"actions": {
+    		"flirt": lambda args: print("The log appears to stiffen.")
+    	},
+    	"weight": 500
     }
 }
